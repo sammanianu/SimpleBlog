145b5b4d84caab1e38f0928d3dedd82af56e3205
Sammai <@sammanianu123@gmail.comgit config --global user.name Sammaigit config --global user.email @sammanianu123@gmail.com>
2018-03-24 21:51:55 +0530

    Main Activity, Post Activity, Blog

---
+++ app/src/main/java/com/example/sanu/simpleblog/Blog.java
@@ -0,0 +1,44 @@
+package com.example.sanu.simpleblog;
+
+/**
+ * Created by sammani on 24/03/2018.
+ */
+
+public class Blog {
+
+    private String title,desc,image;
+
+    public Blog(){
+
+    }
+
+    public Blog(String title, String desc, String image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
